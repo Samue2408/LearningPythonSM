7b66068e0c3325a51687655dafda1153625cece5
Samuel Maldonado <130024600+Samue2408@users.noreply.github.com>
2023-09-06 10:23:17 -0500

    Update reto2.py

--- Ejercicios/reto2.py
+++ Ejercicios/reto2.py
@@ -1,12 +1,10 @@
 #SAMUEL MALDONADO MONTERO
 
-# Pedimos al usuario que introduzca una cadena
 cadena = input("Introduce una palabra o frase: ")
-# Convertimos la cadena a minúsculas y eliminamos los espacios en blanco
+
 cadena = cadena.lower().replace(" ", "")
 
-# Comprobamos si es palíndroma y mostramos un mensaje adecuado
 if cadena == cadena[::-1]:
     print("¡La cadena es palíndroma!", cadena)
 else:
-    print("La cadena no es palíndroma.")
+    print("La cadena no es palíndroma")
